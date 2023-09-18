f14797c2412b19582e70c757c7f6b13bbe780fac
tritri <64111002+trimikix20@users.noreply.github.com>
2023-09-18 14:44:38 -0400

    liste et creation
    afichage de la liste dans index
    
    commencement de la page creatio

--- creerEvenement.php
+++ creerEvenement.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +9,40 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>CREATION EVENEMENT</h1>
+    <?php
+    $nomEvent = "";
+    $nomEventErr = "";
+    $descEvent = "";
+    $descEventErr = "";
+    $dateEvent = "";
+    $dateEventErr = "";
+    $lieuEvent = "";
+    $lieuEventErr = "";
+    $erreur = false;
+    
+if($_SERVER['REQUEST_METHOD'] != "POST" || $erreur == true){
+    echo "<h1>LISTE DES EVENEMENTS</h1>";
+   
+    ?>
+<div class="container-fluid">
+<div class="row col-12">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+    nom de l'evenement : <input type="text" name="nomE"  value="<?php echo $nomEvent;?>">
+    <p style="color:red;"><?php echo $nomEventErr; ?></p>
+    description de l'evenement : <input type="text" name="descE"  value="<?php echo $descEvent;?>">
+    <p style="color:red;"><?php echo $descEventErr; ?></p>
+    date de l'evenement : <input type="date" name="dateE"  value="<?php echo $dateEvent;?>">
+    <p style="color:red;"><?php echo $dateEventErr; ?></p>
+    lieu de l'evenement : <input type="text" name="lieuE"  value="<?php echo $lieuEvent;?>">
+    <p style="color:red;"><?php echo $lieuEventErr; ?></p>
+        <input type="submit" value="Créer l'événement">
+    </form>
+        </div>
+    </div>
+    <?php
+   }
+
+?>
+
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +11,85 @@
 <body>
     <h1>LISTE DES EVENEMENTS</h1>
     
+    <?php
+$nomEvent = "";
+$nomEventErr = "";
+$descEvent = "";
+$descEventErr = "";
+$dateEvent = "";
+$dateEventErr = "";
+$lieuEvent = "";
+$lieuEventErr = "";
+$erreur = false;
+$_SESSION["connexion"] = true;
+
+
+
+if($_SERVER['REQUEST_METHOD'] != "POST" || $erreur == true){
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $db = "appsatisfaction";
+
+    // Create connection  
+    $conn = new mysqli($servername, $username, $password, $db);
+    // Check connection
+    if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+        
+    }
+    $sql = "SELECT * FROM evenement";
+    $result = $conn->query($sql);
+
+
+
+}
+
+?>
+
+<!-- affichage des ellement -->
+<div class="container-fluid">
+<div class="row col-12">
+<table class="table table-striped" >
+  <tr>
+    <th class="tdCat">id</th>
+    <th class="tdCat">nom</th>
+    <th class="tdCat">description</th>
+    <th class="tdCat">date</th>
+    <th class="tdCat">lieu</th>
+    <th class="tdCat">etat</th>
+
+  </tr>
+
+<?php
+if($result->num_rows > 0 ){
+    while($row = $result->fetch_assoc()){
+?>
+<tr>
+            <td class="tdList"><?php echo $row["id"] ?></td>
+            <td class="tdList"><?php echo $row["nom"] ?></td>
+            <td class="tdList"><?php echo $row["description"]?></td>
+            <td class="tdList"><?php echo $row["date"] ?></td>
+            <td class="tdList"><?php echo $row["lieu"] ?></td>
+            <td class="tdList"><?php echo $row["etat"] ?></td>
+            <td class="tdlist"><a href="modifier.php?id=<?php echo $row["id"] ?>">🛠️</a></td>
+            <td class="tdlist"><a href="supprimer.php?id=<?php echo $row["id"] ?>">❌</a></td>
+        </tr>
+        <?php
+    }
+}
+    else{
+        echo "0 resultats";
+    }
+    
+    ?>
+    <!-- fin affichage des ellement -->
+
+</table>
+</div>
+<a href="creerEvenement.php">Créer un événement</a>
+</div>
+
 
 </body>
 </html>
