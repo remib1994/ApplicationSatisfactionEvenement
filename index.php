e4ffe0a1878dfb6a98ec491fe9509e32a7d11a78
Remi Berneche <remib1994@gmail.com>
2023-09-21 15:47:07 -0400

    Ajout bootstrap

--- index.php
+++ index.php
@@ -6,7 +6,10 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="style.css" /> 
+    
     <title>Document</title>
 </head>
 <body>
@@ -41,9 +44,6 @@ if($_SERVER['REQUEST_METHOD'] != "POST" || $erreur == true){
     }
     $sql = "SELECT * FROM evenement";
     $result = $conn->query($sql);
-
-
-
 }
 
 ?>
