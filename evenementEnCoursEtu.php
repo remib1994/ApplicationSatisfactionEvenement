4f71aef12c112e29db76863a167710426b8b0364
tritri <64111002+trimikix20@users.noreply.github.com>
2023-09-21 17:04:21 -0400

    ajout page evenement vote
    ajout des pages evenement etudiant  et personnel et dossier images

---
+++ evenementEnCoursEtu.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body class="bodyEtu">
+    <div class="container">
+        <h1 class="titreEvenCour">VOTE ÉTUDIANT</h1>
+    </div>
+    <div class="container vote">
+        <img src="img/green.png"  class="imgVoteE">
+        <img src="img/yellow.png"  class="imgVoteE">
+        <img src="img/red.png"  class="imgVoteE">
+    </div>
+</body>
+</html>
