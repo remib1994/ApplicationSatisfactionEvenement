d64be301783f25c5f62ebe17273d7646f2a26663
tritri <64111002+trimikix20@users.noreply.github.com>
2023-09-25 14:47:44 -0400

    affichage et sql

--- FichiersBD/appsatisfaction.sql
+++ FichiersBD/appsatisfaction.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.4.1
+-- version 4.2.7.1
 -- http://www.phpmyadmin.net
 --
--- Client :  localhost
--- Généré le :  Lun 18 Septembre 2023 à 18:01
--- Version du serveur :  5.7.11
--- Version de PHP :  5.6.18
+-- Host: localhost
+-- Generation Time: Sep 25, 2023 at 06:44 PM
+-- Server version: 5.6.20-log
+-- PHP Version: 7.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,26 +14,26 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
--- Base de données :  `appsatisfaction`
+-- Database: `appsatisfaction`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `departement`
+-- Table structure for table `departement`
 --
 
-CREATE TABLE `departement` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `departement` (
+`id` int(11) NOT NULL,
   `code` varchar(11) CHARACTER SET utf16 COLLATE utf16_bin NOT NULL,
   `nom` varchar(75) CHARACTER SET utf16 COLLATE utf16_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=41 ;
 
 --
--- Contenu de la table `departement`
+-- Dumping data for table `departement`
 --
 
 INSERT INTO `departement` (`id`, `code`, `nom`) VALUES
@@ -50,10 +50,10 @@ INSERT INTO `departement` (`id`, `code`, `nom`) VALUES
 (11, '551', 'Musique'),
 (12, '601', 'Littérature et communication'),
 (13, '604', 'Langue'),
-(14, '111.A0', 'Techniques d\'hygiène dentaire'),
+(14, '111.A0', 'Techniques d''hygiène dentaire'),
 (15, '120.A0', 'Techniques de diététique'),
 (16, '180.A0', 'Techniques  de soins infirmiers'),
-(17, '221.A0', 'Technologie de l\'architecture'),
+(17, '221.A0', 'Technologie de l''architecture'),
 (18, '221.B0', 'Technologie du génie civil'),
 (19, '221.C0', 'Technologie de la mécanique du bâtiment'),
 (20, '241.A0', 'Techniques de génie mécanique'),
@@ -61,12 +61,12 @@ INSERT INTO `departement` (`id`, `code`, `nom`) VALUES
 (22, '243.00', 'Technologie du génie électrique'),
 (23, '270.00', 'Technologie du génie méttalurgique'),
 (24, '310.A0', 'Techniques  Policières'),
-(25, '322.A1', 'Techniques  d\'éducation à l\'enfance'),
+(25, '322.A1', 'Techniques  d''éducation à l''enfance'),
 (26, '388.A0', 'Techniques de travail social'),
 (27, '393.A0', 'Techniques de la documentation'),
 (28, '410.00', 'Techniques administratives'),
-(29, '420.B0', 'Techniques de l\'informatique'),
-(30, '570.E0', 'Techniques de design d\'intérieur'),
+(29, '420.B0', 'Techniques de l''informatique'),
+(30, '570.E0', 'Techniques de design d''intérieur'),
 (31, '081.06', 'Tremplin - DEC'),
 (32, '200.B0', 'Sciences de la nature'),
 (33, '200.C0', 'Sciences informatiques et mathématiques'),
@@ -81,11 +81,11 @@ INSERT INTO `departement` (`id`, `code`, `nom`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structure de la table `evenement`
+-- Table structure for table `evenement`
 --
 
-CREATE TABLE `evenement` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `evenement` (
+`id` int(11) NOT NULL,
   `nom` varchar(255) COLLATE utf16_bin NOT NULL,
   `description` varchar(255) COLLATE utf16_bin NOT NULL,
   `date` date NOT NULL,
@@ -98,50 +98,61 @@ CREATE TABLE `evenement` (
   `OkAdmin` int(11) NOT NULL DEFAULT '0',
   `BadAdmin` int(11) NOT NULL DEFAULT '0',
   `id_user` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf16 COLLATE=utf16_bin AUTO_INCREMENT=7 ;
 
 --
--- Contenu de la table `evenement`
+-- Dumping data for table `evenement`
 --
 
 INSERT INTO `evenement` (`id`, `nom`, `description`, `date`, `lieu`, `etat`, `Good`, `Ok`, `Bad`, `GoodAdmin`, `OkAdmin`, `BadAdmin`, `id_user`) VALUES
-(1, 'Party d\'intégration 2023', 'Le party de la technique informatique 420.B0 et de science informatique', '2023-10-04', 'Randolph', 'A venir', 0, 0, 0, 0, 0, 0, 1);
+(1, 'Party d''intégration 2023', 'Le party de la technique informatique 420.B0 et de science informatique', '2023-10-04', 'Randolph', 'a venir', 0, 0, 0, 0, 0, 0, 1),
+(2, 'construction Lego', 'diner construction de lego que vous apporter partager la passion des lego', '2023-09-26', 'iiii', 'en cours', 0, 0, 0, 0, 0, 0, 1),
+(3, 'iracing', 'course de simulation en realiter virtuel', '2023-09-21', 'salle informatique', 'terminer', 26, 10, 1, 5, 3, 4, 1),
+(5, 'asas', 'asasas', '2023-09-13', 'asasas', 'asasas', 0, 0, 0, 0, 0, 0, 1),
+(6, 'asas', 'asasas', '2023-09-13', 'asasas', 'asasas', 0, 0, 0, 0, 0, 0, 1);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `evenement_dept`
+-- Table structure for table `evenement_dept`
 --
 
-CREATE TABLE `evenement_dept` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `evenement_dept` (
+`id` int(11) NOT NULL,
   `id_Evenement` int(11) NOT NULL,
   `id_Departement` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf16 COLLATE=utf16_bin AUTO_INCREMENT=10 ;
 
 --
--- Contenu de la table `evenement_dept`
+-- Dumping data for table `evenement_dept`
 --
 
 INSERT INTO `evenement_dept` (`id`, `id_Evenement`, `id_Departement`) VALUES
 (1, 1, 29),
-(2, 1, 33);
+(2, 1, 33),
+(3, 2, 26),
+(4, 2, 17),
+(5, 3, 4),
+(6, 3, 9),
+(7, 3, 17),
+(8, 3, 26),
+(9, 3, 15);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `user`
+-- Table structure for table `user`
 --
 
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `user` (
+`id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(2000) CHARACTER SET utf16 COLLATE utf16_bin NOT NULL,
   `email` varchar(75) CHARACTER SET utf16 COLLATE utf16_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
--- Contenu de la table `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `email`) VALUES
@@ -149,80 +160,73 @@ INSERT INTO `user` (`id`, `username`, `password`, `email`) VALUES
 (2, 'shany', '536b7eca17b99da93485ae5046bfc9ef2e470f14', 'shany.carle@cegeptr.qc.ca');
 
 --
--- Index pour les tables exportées
+-- Indexes for dumped tables
 --
 
 --
--- Index pour la table `departement`
+-- Indexes for table `departement`
 --
 ALTER TABLE `departement`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `nom` (`nom`);
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `nom` (`nom`);
 
 --
--- Index pour la table `evenement`
+-- Indexes for table `evenement`
 --
 ALTER TABLE `evenement`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_user` (`id_user`),
-  ADD KEY `id_user_2` (`id_user`);
+ ADD PRIMARY KEY (`id`), ADD KEY `id_user` (`id_user`), ADD KEY `id_user_2` (`id_user`);
 
 --
--- Index pour la table `evenement_dept`
+-- Indexes for table `evenement_dept`
 --
 ALTER TABLE `evenement_dept`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_id_departement` (`id_Departement`),
-  ADD KEY `fk_id_evenement` (`id_Evenement`);
+ ADD PRIMARY KEY (`id`), ADD KEY `fk_id_departement` (`id_Departement`), ADD KEY `fk_id_evenement` (`id_Evenement`);
 
 --
--- Index pour la table `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `email` (`email`);
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `username` (`username`), ADD UNIQUE KEY `email` (`email`);
 
 --
--- AUTO_INCREMENT pour les tables exportées
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT pour la table `departement`
+-- AUTO_INCREMENT for table `departement`
 --
 ALTER TABLE `departement`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=41;
 --
--- AUTO_INCREMENT pour la table `evenement`
+-- AUTO_INCREMENT for table `evenement`
 --
 ALTER TABLE `evenement`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 --
--- AUTO_INCREMENT pour la table `evenement_dept`
+-- AUTO_INCREMENT for table `evenement_dept`
 --
 ALTER TABLE `evenement_dept`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 --
--- AUTO_INCREMENT pour la table `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
--- Contraintes pour les tables exportées
+-- Constraints for dumped tables
 --
 
 --
--- Contraintes pour la table `evenement`
+-- Constraints for table `evenement`
 --
 ALTER TABLE `evenement`
-  ADD CONSTRAINT `fk_id_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
+ADD CONSTRAINT `fk_id_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
 
 --
--- Contraintes pour la table `evenement_dept`
+-- Constraints for table `evenement_dept`
 --
 ALTER TABLE `evenement_dept`
-  ADD CONSTRAINT `fk_id_departement` FOREIGN KEY (`id_Departement`) REFERENCES `departement` (`id`),
-  ADD CONSTRAINT `fk_id_evenement` FOREIGN KEY (`id_Evenement`) REFERENCES `evenement` (`id`);
+ADD CONSTRAINT `fk_id_departement` FOREIGN KEY (`id_Departement`) REFERENCES `departement` (`id`),
+ADD CONSTRAINT `fk_id_evenement` FOREIGN KEY (`id_Evenement`) REFERENCES `evenement` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- index.php
+++ index.php
@@ -6,10 +6,11 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <script src="js/bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="style.css" /> 
-    
+   
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="script.js"></script>
     <title>Document</title>
 </head>
 <body>
@@ -42,15 +43,26 @@ if($_SERVER['REQUEST_METHOD'] != "POST" || $erreur == true){
     die("Connection failed: " . $conn->connect_error);
         
     }
-    $sql = "SELECT * FROM evenement";
+    $sql = "SELECT * FROM evenement WHERE etat = 'terminer'";
     $result = $conn->query($sql);
+    $sql2 = "SELECT * FROM evenement WHERE etat = 'en cours'";
+    $result2 = $conn->query($sql2);
+    $sql3 = "SELECT * FROM evenement WHERE etat = 'a venir'";
+    $result3 = $conn->query($sql3);
+
+
+
 }
 
 ?>
+<!-- affichage des ellement -->
 
 <!-- affichage des ellement -->
 <div class="container-fluid">
 <div class="row col-12">
+
+<button class="accordion">terminer</button>
+<div class="panel">
 <table class="table table-striped" >
   <tr>
     <th class="tdCat">id</th>
@@ -58,40 +70,187 @@ if($_SERVER['REQUEST_METHOD'] != "POST" || $erreur == true){
     <th class="tdCat">description</th>
     <th class="tdCat">date</th>
     <th class="tdCat">lieu</th>
+    <th class="tdCat">departement</th>
     <th class="tdCat">etat</th>
 
   </tr>
-
-<?php
+  <?php
 if($result->num_rows > 0 ){
     while($row = $result->fetch_assoc()){
-?>
-<tr>
+?> 
+<tr >
             <td class="tdList"><?php echo $row["id"] ?></td>
             <td class="tdList"><?php echo $row["nom"] ?></td>
             <td class="tdList"><?php echo $row["description"]?></td>
             <td class="tdList"><?php echo $row["date"] ?></td>
             <td class="tdList"><?php echo $row["lieu"] ?></td>
-            <td class="tdList"><?php echo $row["etat"] ?></td>
+            <td class="tdList">
+           <?php $sql11 = 'SELECT nom,code
+FROM departement
+WHERE id IN (
+SELECT id_departement
+FROM evenement_dept
+WHERE id_Evenement =' . $row["id"] . ')';
+    $result11 = $conn->query($sql11);
+    if($result11->num_rows > 0 ){
+        while($row11 = $result11->fetch_assoc()){
+            ?>
+            <?php
+     echo $row11["code" ]." ".$row11["nom" ]."<br>";
+       }
+    }
+        ?></td>
+     
+     
+    
+    
+            <td class="tdList" ><?php echo $row["etat"] ?></td>
             <td class="tdlist"><a href="modifier.php?id=<?php echo $row["id"] ?>">🛠️</a></td>
             <td class="tdlist"><a href="supprimer.php?id=<?php echo $row["id"] ?>">❌</a></td>
         </tr>
-       
+        
         <?php
     }
 }
-    else{
-        echo "0 resultats";
-    }
-    
-    ?>
-    <!-- fin affichage des ellement -->
-
+?>
 </table>
 </div>
+
+<button class="accordion">En cours</button>
+<div class="panel">
+<table class="table table-striped" >
+  <tr>
+    <th class="tdCat">id</th>
+    <th class="tdCat">nom</th>
+    <th class="tdCat">description</th>
+    <th class="tdCat">date</th>
+    <th class="tdCat">lieu</th>
+    <th class="tdCat">departement</th>
+    <th class="tdCat">etat</th>
+
+  </tr>
+  <?php
+if($result2->num_rows > 0 ){
+    while($row2 = $result2->fetch_assoc()){
+?> 
+<tr >
+            <td class="tdList"><?php echo $row2["id"] ?></td>
+            <td class="tdList"><?php echo $row2["nom"] ?></td>
+            <td class="tdList"><?php echo $row2["description"]?></td>
+            <td class="tdList"><?php echo $row2["date"] ?></td>
+            <td class="tdList"><?php echo $row2["lieu"] ?></td>
+            <td class="tdList">
+           <?php $sql22 = 'SELECT nom,code
+FROM departement
+WHERE id IN (
+SELECT id_departement
+FROM evenement_dept
+WHERE id_Evenement =' . $row2["id"] . ')';
+    $result22 = $conn->query($sql22);
+    if($result22->num_rows > 0 ){
+        while($row22 = $result22->fetch_assoc()){
+            ?>
+            <?php
+     echo $row22["code" ]." ".$row22["nom" ]."<br>";
+       }
+    }
+        ?></td>
+     
+     
+    
+    
+            <td class="tdList" ><?php echo $row2["etat"] ?></td>
+            <td class="tdlist"><a href="modifier.php?id=<?php echo $row2["id"] ?>">🛠️</a></td>
+            <td class="tdlist"><a href="supprimer.php?id=<?php echo $row2["id"] ?>">❌</a></td>
+        </tr>
+        
+        <?php
+    }
+}
+?>
+</table>
+</div>
+
+
+<button class="accordion">A venir</button>
+<div class="panel">
+<table class="table table-striped" >
+  <tr>
+    <th class="tdCat">id</th>
+    <th class="tdCat">nom</th>
+    <th class="tdCat">description</th>
+    <th class="tdCat">date</th>
+    <th class="tdCat">lieu</th>
+    <th class="tdCat">departement</th>
+    <th class="tdCat">etat</th>
+
+  </tr>
+  <?php
+if($result3->num_rows > 0 ){
+    while($row3 = $result3->fetch_assoc()){
+?> 
+<tr >
+            <td class="tdList"><?php echo $row3["id"] ?></td>
+            <td class="tdList"><?php echo $row3["nom"] ?></td>
+            <td class="tdList"><?php echo $row3["description"]?></td>
+            <td class="tdList"><?php echo $row3["date"] ?></td>
+            <td class="tdList"><?php echo $row3["lieu"] ?></td>
+            <td class="tdList">
+           <?php $sql33 = 'SELECT nom,code
+FROM departement
+WHERE id IN (
+SELECT id_departement
+FROM evenement_dept
+WHERE id_Evenement =' . $row3["id"] . ')';
+    $result33 = $conn->query($sql33);
+    if($result33->num_rows > 0 ){
+        while($row33 = $result33->fetch_assoc()){
+            ?>
+            <?php
+     echo $row33["code" ]." ".$row33["nom" ]."<br>";
+       }
+    }
+        ?></td>
+     
+     
+    
+    
+            <td class="tdList" ><?php echo $row3["etat"] ?></td>
+            <td class="tdlist"><a href="modifier.php?id=<?php echo $row3["id"] ?>">🛠️</a></td>
+            <td class="tdlist"><a href="supprimer.php?id=<?php echo $row3["id"] ?>">❌</a></td>
+        </tr>
+        
+        <?php
+    }
+}
+?>
+</table>
+</div>
+
+
+ 
+    
+   
+
+
 <a href="creerEvenement.php">Créer un événement</a>
 </div>
 
+<script>
+var acc = document.getElementsByClassName("accordion");
+var i;
 
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+</script>
 </body>
 </html>
